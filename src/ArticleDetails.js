1ff1d61226c7b6b1c647027a009a1545b0e0320b
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-12 11:50:07 -0600

    Contruct layout for articledetail and display in app

---
+++ src/ArticleDetails.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const ArticleDetails = () => {
+  return (
+    <div>
+      <NavBar />
+    <section>
+      <h3>Publisher Name</h3>
+      <p>Publish Date</p>
+      <p>Another image???</p>
+    </section>
+    </div>
+  )
+}
+
+
+export default ArticleDetails;
