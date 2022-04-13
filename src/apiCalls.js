4ad2a4d85d920b55d2abbc11599907ff89c2ac2a
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-13 15:24:06 -0600

    Add return to api fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchArticles = () => {
-  fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=1Qzvpl0KwcglnxxeAwe5oNNZMwjTADYj')
+  return fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=1Qzvpl0KwcglnxxeAwe5oNNZMwjTADYj')
   .then(response => response.json())
 }
 
