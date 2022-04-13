cd26c8518960be3849132a55850b5abe488f97e4
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-13 17:32:10 -0600

    Remove nav bar from function

--- src/ArticleDetails.js
+++ src/ArticleDetails.js
@@ -3,14 +3,11 @@ import NavBar from './NavBar';
 
 const ArticleDetails = () => {
   return (
-    <div>
-      <NavBar />
     <section>
       <h3>Publisher Name</h3>
       <p>Publish Date</p>
       <p>Another image???</p>
     </section>
-    </div>
   )
 }
 
