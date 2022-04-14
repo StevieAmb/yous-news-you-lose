ea1cd5f9151bbaa1d45360c0ff3d45b8066506d1
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-14 10:45:53 -0600

    Populate component with api data to display to page

--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Article = ({title, img, abstract}) => {
+const Article = ({id, title, img, abstract}) => {
   return (
     <article>
       <h3>{title}</h3>
       <img src={img}/>
       <p>{abstract}</p>
+      <NavLink to={`/article/${id}`}>
       <button>More Details Here</button>
+      </NavLink>
     </article>
   )
 }
