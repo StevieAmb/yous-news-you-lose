e609ec89055216f784e3af7867b0aff9b08d4675
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-14 18:44:46 -0600

    Create layout for filtered articles

---
+++ src/FiltArticle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+ const FiltArticle = ({id, title, img, abstract}) => {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <img src={img}/>
+      <p>{abstract}</p>
+      <NavLink to={`/article/${id}`}>
+      <button>More Details Here</button>
+      </NavLink>
+    </article>
+  )
+ }
+
+
+export default FiltArticle;
