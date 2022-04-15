bf831ff0a34aa8b091b406265158dc4dfb285a44
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-14 18:49:32 -0600

    Construct article card to be displayed on page

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Article.css';
 
 const Article = ({id, title, img, abstract}) => {
   return (
-    <article>
+    <article className='article-card'>
       <h3>{title}</h3>
       <img src={img}/>
       <p>{abstract}</p>
